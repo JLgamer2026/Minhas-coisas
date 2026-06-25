1bad29ff518890f6fee52d1159dbbef36ad2e22f
JLgamer2026 <jlms082010@yahoo.com>
2026-06-25 17:19:16 -0300

    commit teste

---
+++ sotestandoocommit.py
@@ -0,0 +1,5 @@
+from pybricks.hubs import PrimeHub 
+from pybricks.pupdevices import Motor, UltrasonicSensor, ColorSensor 
+from pybricks.parameters import Color, Port, Direction 
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase  
