fe389dd5d49f8dfba60b72c660e93d59a01dec85
JoaquimCaetanoCarvalho <joaquim.caetano460@gmail.com>
2026-07-14 15:47:28 -0300

    iniciação do codigo da area de resgate

---
+++ OBR _2026/area_resgate.py
@@ -0,0 +1,6 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, UltrasonicSensor
+from pybricks.parameters import Port, Direction, Color
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
